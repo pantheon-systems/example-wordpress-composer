b6631127f48c5b9b4ec19b94dd7e2e0ff4427895
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-01-01 12:36:40 -0800

    Print out the page contents after any failed test.

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -6,6 +6,7 @@ use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Drupal\DrupalExtension\Context\MinkContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Hook\Scope\AfterStepScope;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
@@ -212,5 +213,38 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
         );
 
         return false;
+    }
+
+    /**
+     * @AfterStep
+     */
+    public function afterStep(AfterStepScope $scope)
+    {
+        // Do nothing on steps that pass
+        $result = $scope->getTestResult();
+        if ($result->isPassed()) {
+            return;
+        }
+
+        // Otherwise, dump the page contents.
+        $session = $this->getSession();
+        $page = $session->getPage();
+        $html = $page->getContent();
+        $html = static::trimHead($html);
+
+        print "::::::::::::::::::::::::::::::::::::::::::::::::\n";
+        print $html . "\n";
+        print "::::::::::::::::::::::::::::::::::::::::::::::::\n";
+    }
+
+    /**
+     * Remove everything in the '<head>' element except the
+     * title, because it is long and uninteresting.
+     */
+    protected static function trimHead($html)
+    {
+        $html = preg_replace('#\<head\>.*\<title\>#sU', '<head><title>', $html);
+        $html = preg_replace('#\</title\>.*\</head\>#sU', '</title></head>', $html);
+        return $html;
     }
 }
