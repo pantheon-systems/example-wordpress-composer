545acea1a7a222982ab3135c6a89b5073e8841d7
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-01-08 12:53:26 -0800

    Use a temporary scratch repository. (#39)
    * Use a temporary scratch repository.
    
    * Add config_direct_save module.
    
    * Fetch the repository url from the build metadata rather than the github secrets. Insert the github token from the github secrets file into the repository url.
    
    * Set the secrets in a different location (n.b. tests wipe the site, which erases secrets).
    
    * Push over same remote branch if no commits have been added since the environment was created.
    
    * Improve comment in .gitignore regarding build assets.
    
    * Omit the final push if applying the code change from the the isolated commit to the work branch (the HEAD commit from the upstream repository, just prior to the commit containing the build artifacts) results in any sort of error or conflict. Note that this should never happen, as all of the build artifacts should be .gitignore'd.

--- web/private/scripts/sync_code/push-back-to-github.php
+++ web/private/scripts/sync_code/push-back-to-github.php
@@ -1,5 +1,10 @@
 <?php
 
+// This is only for multidev environments.
+if (in_array($_ENV['PANTHEON_ENVIRONMENT'], ['dev', 'test', 'live'])) {
+  return;
+}
+
 /**
  * This script will attempt to push "lean" changes back upstream.
  */
@@ -16,6 +21,10 @@ if (!file_exists($buildMetadataFile)) {
 }
 $buildMetadataFileContents = file_get_contents($buildMetadataFile);
 $buildMetadata = json_decode($buildMetadataFileContents, true);
+if (empty($buildMetadata)) {
+  print "No data in build metadata\n";
+  return;
+}
 
 print "::::::::::::::::: Build Metadata :::::::::::::::::\n";
 var_export($buildMetadata);
@@ -23,15 +32,28 @@ print "\n\n";
 
 $privateFiles = "$bindingDir/files/private";
 $gitHubSecretsFile = "$privateFiles/github-secrets.json";
+if (!file_exists($privateFiles)) {
+  print "Could not find $gitHubSecretsFile\n";
+  return;
+}
 $gitHubSecretsContents = file_get_contents($gitHubSecretsFile);
 $gitHubSecrets = json_decode($gitHubSecretsContents, true);
+if (empty($gitHubSecrets)) {
+  print "No data in GitHub secrets\n";
+  return;
+}
 
 print "::::::::::::::::: GitHub Secrets :::::::::::::::::\n";
 var_export($gitHubSecrets);
 print "\n\n";
 
 // The remote repo to push to
-$upstreamRepo = $gitHubSecrets['repo']['url'];
+$upstreamRepo = $buildMetadata['url'];
+if (!empty($gitHubSecrets) && array_key_exists('token', $gitHubSecrets)) {
+  $token = $gitHubSecrets['token'];
+  $upstreamRepo = str_replace('git@github.com:', 'https://github.com/', $upstreamRepo);
+  $upstreamRepo = str_replace('https://', "https://$token:x-oauth-basic@", $upstreamRepo);
+}
 
 // The last commit made on the lean repo prior to creating the build artifacts
 $fromSha = $buildMetadata['sha'];
@@ -39,27 +61,60 @@ $fromSha = $buildMetadata['sha'];
 // The name of the PR branch
 $branch = $buildMetadata['ref'];
 
-// The name of our new branch
-$prBranch = "new-$branch";
+// A working branch to make changes on
+$workBranch = substr($currentCommit, 0, 5) . $branch;
 
 // The commit to cherry-pick
 $currentCommit = exec('git rev-parse HEAD');
 
-// TODO: vet the contents of the commit for applicability first.
-// If the commit is 'mixed', it must be rejected. Is there any
-// way to recover from this? Maybe not.
-
 print "::::::::::::::::: Info :::::::::::::::::\n";
-print "We are going to check out $prBranch from $fromSha, then cherry-pick $currentCommit and push it back to $upstreamRepo\n";
+print "We are going to check out $branch from $fromSha, then cherry-pick $currentCommit and push it back to {$buildMetadata['url']}\n";
 
-// Create our new branch
-passthru("git checkout -B $prBranch $fromSha");
+// Create our new branch without switching to it. We start with
+// '$fromSha' to avoid placing any build artifacts on our branch.
+passthru("git -C $repositoryRoot branch -f $workBranch $fromSha");
 
-// Cherry-pick the commit just made
-passthru("git cherry-pick -Xthiers $currentCommit");
+$pantheonRepository = "file://$repositoryRoot";
+$workRepository = "$bindingDir/tmp/scratchRepository";
 
-// Go back to the branch we were on before
-passthru('git checkout -');
+// Clone our current repository -- but only take the current branch.
+// We need a separate working tree because we do not want to alter the
+// current repository, which is actively in use by the current multidev
+// environment. Git requires a modifiable working tree to cherry-pick commits.
+passthru("git clone $pantheonRepository --branch $workBranch --single-branch $workRepository");
 
-// Push the new branch back to Pantheon
-passthru("git push $upstreamRepo $prBranch");
+// Use show | apply to do the equivalent of a cherry-pick
+// between the two repositories.
+exec("git -C $repositoryRoot show $currentCommit | git -c $workRepository apply -Xthiers", $output, $applyStatus);
+
+// We're done with the work branch now.
+passthru("git -C $repositoryRoot branch -D $workBranch");
+
+// If the apply worked, then push the commit back to the light repository.
+if ($applyStatus == 0) {
+  // Get the sha commit hash of the remote GitHub branch, to see if
+  // anyone has added any commits there since this environment was created.
+  // This comes back as "sha   refs/heads/branch", so we'll trim the end.
+  $remoteCommit = exec("git ls-remote $upstreamRepo $branch");
+  $remoteCommit = preg_replace('/ .*/', '', $remoteCommit);
+
+  // If the remote commit is the same as our base commit, then we will
+  // push directly back to the original branch. If it does not match, then
+  // we will push to a new branch name for the user to merge on GitHub.
+  $targetBranch = $workBranch;
+  if ($remoteCommit == $fromSha) {
+    $targetBranch = $branch;
+  }
+
+  // Push the new branch back to Pantheon
+  passthru("git -C $workRepository push $upstreamRepo $workBranch:$targetBranch");
+}
+
+// We don't need the work branch or the second working repository any longer
+passthru("rm -rf $workRepository");
+
+// Post error to dashboard and exit if the merge fails.
+if ($applyStatus != 0) {
+  $message = "git apply failed with exit code $applyStatus.\n\n" . explode("\n", $output);
+  pantheon_raise_dashboard_error($message, true);
+}
