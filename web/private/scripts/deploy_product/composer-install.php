9f390044b65bc1c406d42e19ae9f0f91d72f441c
Steve Persch <stevector@users.noreply.github.com>
2017-06-06 20:48:54 -0500

    WIP - deleting unused files (#17)
    * Delete FeatureContext.php
    
    * Delete add-commit-comment
    
    * Delete content.feature
    
    * [ci skip]
    
    * More deletion of unused files

--- web/private/scripts/deploy_product/composer-install.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-chdir(getenv('HOME') . '/code');
-
-print "\n====== Running 'composer install' ======\n\n";
-passthru('composer install 2>&1');
--- web/private/scripts/deploy_product/prepare-for-pantheon.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-chdir(getenv('HOME') . '/code');
-
-print "\n====== Running 'composer prepare-for-pantheon' ======\n\n";
-passthru('composer prepare-for-pantheon 2>&1');
