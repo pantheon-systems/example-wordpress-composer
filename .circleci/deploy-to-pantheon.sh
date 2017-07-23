568607d67754f79ed134dbb91a79c05cddccc339
ataylorme <andrew@ataylor.me>
2017-07-23 12:24:14 -0400

    Only create multidevs for pull requests and use pull request number for multidev name (rather than branch name)

--- .circleci/deploy-to-pantheon.sh
+++ .circleci/deploy-to-pantheon.sh
@@ -2,17 +2,43 @@
 
 set -ex
 
+# Stash list of Pantheon multidev environments
+PANTHEON_MULTIDEV_LIST="$(terminus multidev:list ${PANTHEON_SITE_UUID} --format=list --field=Name)"
+
 if [[ $CIRCLE_BRANCH == "master" ]]
 then
     terminus build:env:merge -n "$TERMINUS_SITE.$TERMINUS_ENV" --yes
-    terminus build:env:delete:pr -n "$TERMINUS_SITE" --yes
 else
-    # Cleanup old multidevs
-    terminus build:env:delete:ci -n "$TERMINUS_SITE" --keep=2 --yes
+    # If we are not on master or PR abort
+    if [[ -n ${CIRCLE_PULL_REQUEST+x} ]]
+    then
 
-    # Wake the dev environment
-    terminus env:wake -n "$TERMINUS_SITE.dev"
-
-    # Create a new multidev
-    terminus build:env:create -n "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --clone-content --yes --notify="$NOTIFY"
+        # Create a new multidev if needed
+        if ! terminusMultiDevExists ${TERMINUS_ENV}
+        then
+            # Wake dev so we can clone the database
+            terminus env:wake -n "$TERMINUS_SITE.dev"
+            terminus build:env:create -n "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --clone-content --yes --notify="$NOTIFY"
+        else
+            # Otherwise push code to the existing multidev
+            terminus build-env:push-code -n $TERMINUS_SITE.$TERMINUS_ENV
+        fi
+    else
+        echo -e "CircleCI will only deploy to Pantheon for master or pull requests.\n"
+    fi
 fi
+
+# Cleanup old multidevs
+terminus build:env:delete:pr -n "$TERMINUS_SITE" --preserve-prs --delete-branch --yes
+
+terminusMultiDevExists()
+{
+    while read -r multiDev; do
+        if [[ "${multiDev}" == "$1" ]]
+        then
+            return 0;
+        fi
+    done <<< "$PANTHEON_MULTIDEV_LIST"
+
+    return 1;
+}
--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -49,7 +49,17 @@ echo 'export PATH=$PATH:$HOME/bin:$HOME/terminus/bin' >> $BASH_ENV
 echo 'export BRANCH=$(echo $CIRCLE_BRANCH | grep -v '"'"'^\(master\|[0-9]\+.x\)$'"'"')' >> $BASH_ENV
 echo 'export PR_ENV=${BRANCH:+pr-$BRANCH}' >> $BASH_ENV
 echo 'export CIRCLE_ENV=ci-$CIRCLE_BUILD_NUM' >> $BASH_ENV
-echo 'export DEFAULT_ENV=$(echo ${PR_ENV:-$CIRCLE_ENV} | tr '"'"'[:upper:]'"'"' '"'"'[:lower:]'"'"' | sed '"'"'s/[^0-9a-z-]//g'"'"' | cut -c -11 | sed '"'"'s/-$//'"'"')' >> $BASH_ENV
+# If we are on a pull request
+if [[ $CIRCLE_BRANCH != "master" && -n ${CIRCLE_PULL_REQUEST+x} ]]
+then
+	# Then use a pr- branch/multidev
+	PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+	PR_BRANCH="pr-${PR_NUMBER}"
+	echo "export DEFAULT_ENV=$(pr-${PR_NUMBER})" >> $BASH_ENV
+else
+	# otherwise make the branch name multidev friendly
+	echo 'export DEFAULT_ENV=$(echo ${PR_ENV:-$CIRCLE_ENV} | tr '"'"'[:upper:]'"'"' '"'"'[:lower:]'"'"' | sed '"'"'s/[^0-9a-z-]//g'"'"' | cut -c -11 | sed '"'"'s/-$//'"'"')' >> $BASH_ENV
+fi
 echo 'export TERMINUS_ENV=${TERMINUS_ENV:-$DEFAULT_ENV}' >> $BASH_ENV
 source $BASH_ENV
 #===========================================
