63c6d884217e1955b86ae75134ebf66f4416d3e9
ataylorme <andrew@ataylor.me>
2017-07-23 15:11:58 -0400

    Fix coding standard violations

--- web/wp-content/mu-plugins/alter-wpcfm-config-path.php
+++ web/wp-content/mu-plugins/alter-wpcfm-config-path.php
@@ -4,19 +4,29 @@
  * Plugin URI: http://www.eyesopen.ca
  * Description: Alters the wpcfm config path
  * Version: 0.1
- * Author: Grant McInnes
- * Author URI: http://www.eyesopen.ca
+ * Author: Andrew Taylor
+ * Author URI: https://pantheon.io
+ * PHP Version: 7.0
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package AdvancedWordPressOnPantheon
- * @subpackage TwentySeventeenChildTheme
+ * @category Must_Use_Plugin
+ * @package  ExampleWordPressComposer
+ * @author   ataylorme <andrew@pantheon.io>
+ * @license  GPL-2.0+ http://www.gnu.org/licenses/gpl-2.0.txt
+ * @link     https://pantheon.io
  */
 
 /**
  * Tell wp-cfm where our config files live
  */
-add_filter( 'wpcfm_config_dir', function( $var ) {
-	return DOCROOT . 'private/config';
-} );
-add_filter( 'wpcfm_config_url', function( $var ) {
-	return WP_HOME . '/private/config';
-} );
+add_filter(
+    'wpcfm_config_dir', function ( $var ) {
+        return DOCROOT . 'private/config';
+    } 
+);
+add_filter(
+    'wpcfm_config_url', function ( $var ) {
+        return WP_HOME . '/private/config';
+    } 
+);
