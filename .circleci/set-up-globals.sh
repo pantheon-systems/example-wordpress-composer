5160e6d210fedd40dd28fa78ec79f5da715e5040
ataylorme <andrew@ataylor.me>
2017-07-27 16:40:53 -0500

    Revert to exporting variables to BASH_ENV

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -27,27 +27,28 @@ composer global require -n "hirak/prestissimo:^0.3"
 # See: https://discuss.circleci.com/t/environment-variable-expansion-in-working-directory/11322
 # See: https://discuss.circleci.com/t/circle-2-0-global-environment-variables/8681
 #=====================================================================================================================
-export PATH=$PATH:$HOME/bin:$HOME/terminus/bin:$HOME/.composer/vendor/bin
-export BRANCH=$(echo $CIRCLE_BRANCH | grep -v '^\(master\|[0-9]\+.x\)$')
-export PR_ENV=${BRANCH:+pr-$BRANCH}
-export CIRCLE_ENV=ci-$CIRCLE_BUILD_NUM
+echo 'export PATH=$PATH:$HOME/bin:$HOME/terminus/bin' >> $BASH_ENV
+echo 'export BRANCH=$(echo $CIRCLE_BRANCH | grep -v '"'"'^\(master\|[0-9]\+.x\)$'"'"')' >> $BASH_ENV
+echo 'export PR_ENV=${BRANCH:+pr-$BRANCH}' >> $BASH_ENV
+echo 'export CIRCLE_ENV=ci-$CIRCLE_BUILD_NUM' >> $BASH_ENV
 # If we are on a pull request
 if [[ $CIRCLE_BRANCH != "master" && -n ${CIRCLE_PULL_REQUEST+x} ]]
 then
 	# Then use a pr- branch/multidev
-	export PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
-	export PR_BRANCH="pr-${PR_NUMBER}"
-	export DEFAULT_ENV=pr-${PR_NUMBER}
+	PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+	PR_BRANCH="pr-${PR_NUMBER}"
+	echo "export DEFAULT_ENV=pr-${PR_NUMBER}" >> $BASH_ENV
 else
 	# otherwise make the branch name multidev friendly
 	if [[ $CIRCLE_BRANCH == "master" ]]
 	then
-		export DEFAULT_ENV=dev
+		echo "export DEFAULT_ENV=dev" >> $BASH_ENV
 	else
-		export DEFAULT_ENV=$(echo ${PR_ENV:-$CIRCLE_ENV} | tr '[:upper:]' '[:lower:]' | sed 's/[^0-9a-z-]//g' | cut -c -11 | sed 's/-$//')
+		echo 'export DEFAULT_ENV=$(echo ${PR_ENV:-$CIRCLE_ENV} | tr '"'"'[:upper:]'"'"' '"'"'[:lower:]'"'"' | sed '"'"'s/[^0-9a-z-]//g'"'"' | cut -c -11 | sed '"'"'s/-$//'"'"')' >> $BASH_ENV
 	fi
 fi
-export TERMINUS_ENV=${TERMINUS_ENV:-$DEFAULT_ENV}
+echo 'export TERMINUS_ENV=${TERMINUS_ENV:-$DEFAULT_ENV}' >> $BASH_ENV
+source $BASH_ENV
 
 #===========================================
 # End EXPORTing needed environment variables
