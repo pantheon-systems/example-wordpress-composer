519a1599d3c876f3973b095fd6cc71bd81b6f177
ataylorme <andrew@ataylor.me>
2017-07-28 09:24:45 -0500

    Only run Behat on Pantheon if on the master branch or pull request

--- .circleci/behat-test.sh
+++ .circleci/behat-test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ (${CIRCLE_BRANCH} != "master" && -z ${CIRCLE_PULL_REQUEST+x}) || (${CIRCLE_BRANCH} == "master" && -n ${CIRCLE_PULL_REQUEST+x}) ]];
+then
+    echo -e "CircleCI will only run Behat tests on Pantheon if on the master branch or creating a pull requests.\n"
+    exit 0;
+fi
+
 # Bail if required environment varaibles are missing
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]
 then
