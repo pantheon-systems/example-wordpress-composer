ec0156718a7e91e8fa0b4a8135d45ac800c346c2
Steve Persch <stevector@users.noreply.github.com>
2017-09-12 15:56:21 -0500

    Rewrite global variables set up
    Brought over from https://github.com/pantheon-systems/example-drops-8-composer/pull/107

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -19,35 +19,35 @@ composer global require -n "hirak/prestissimo:^0.3"
 # Install Terminus into ~/terminus
 /usr/bin/env COMPOSER_BIN_DIR=$HOME/bin composer --working-dir=$HOME require pantheon-systems/terminus "^1"
 
+# Set up environment variables
+
+# By default, we will make the environment name after the circle build number.
+DEFAULT_ENV=ci-$CIRCLE_BUILD_NUM
+
+# If there is a PR number provided, though, then we will use it instead.
+if [[ -n ${CIRCLE_PULL_REQUEST} ]] ; then
+  PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+  DEFAULT_ENV="pr-${PR_NUMBER}"
+fi
+
+# TODO: We could point our build tools scripts somewhere else
+# e.g. to do a local test if TERMINUS_TOKEN is not set.
+
 #=====================================================================================================================
-# Start EXPORTing needed environment variables
+# EXPORT needed environment variables
+#
 # Circle CI 2.0 does not yet expand environment variables so they have to be manually EXPORTed
 # Once environment variables can be expanded this section can be removed
 # See: https://discuss.circleci.com/t/unclear-how-to-work-with-user-variables-circleci-provided-env-variables/12810/11
 # See: https://discuss.circleci.com/t/environment-variable-expansion-in-working-directory/11322
 # See: https://discuss.circleci.com/t/circle-2-0-global-environment-variables/8681
 #=====================================================================================================================
-echo 'export PATH=$PATH:$HOME/bin:$HOME/terminus/bin' >> $BASH_ENV
-echo 'export BRANCH=$(echo $CIRCLE_BRANCH | grep -v '"'"'^\(master\|[0-9]\+.x\)$'"'"')' >> $BASH_ENV
-echo 'export PR_ENV=${BRANCH:+pr-$BRANCH}' >> $BASH_ENV
-echo 'export CIRCLE_ENV=ci-$CIRCLE_BUILD_NUM' >> $BASH_ENV
-# If we are on a pull request
-if [[ $CIRCLE_BRANCH != "master" && -n ${CIRCLE_PULL_REQUEST+x} ]]
-then
-	# Then use a pr- branch/multidev
-	PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
-	PR_BRANCH="pr-${PR_NUMBER}"
-	echo "export DEFAULT_ENV=pr-${PR_NUMBER}" >> $BASH_ENV
-else
-	# otherwise make the branch name multidev friendly
-	if [[ $CIRCLE_BRANCH == "master" ]]
-	then
-		echo "export DEFAULT_ENV=dev" >> $BASH_ENV
-	else
-		echo 'export DEFAULT_ENV=$(echo ${PR_ENV:-$CIRCLE_ENV} | tr '"'"'[:upper:]'"'"' '"'"'[:lower:]'"'"' | sed '"'"'s/[^0-9a-z-]//g'"'"' | cut -c -11 | sed '"'"'s/-$//'"'"')' >> $BASH_ENV
-	fi
-fi
-echo 'export TERMINUS_ENV=${TERMINUS_ENV:-$DEFAULT_ENV}' >> $BASH_ENV
+(
+  echo 'export PATH=$PATH:$HOME/bin'
+  echo "export DEFAULT_ENV='$DEFAULT_ENV'"
+  echo 'export TERMINUS_ENV=${TERMINUS_ENV:-$DEFAULT_ENV}'
+  echo "export BUILD_TOOLS_SCRIPTS='$BUILD_TOOLS_SCRIPTS'"
+) >> $BASH_ENV
 source $BASH_ENV
 
 #===========================================
