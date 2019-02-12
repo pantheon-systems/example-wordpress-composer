5b37d82edf7030dc9d007328bccaa7c9cf11fb19
ataylorme <andrew@ataylor.me>
2019-02-12 09:46:35 -0800

    Add Gitlab CI file

--- .ci/behat-test.sh
+++ .ci/behat-test.sh
@@ -13,10 +13,11 @@ then
   exit 1
 fi
 
+# Bail if no admin username/password are set
 if [ -z "$ADMIN_USERNAME" ] || [ -z "$ADMIN_PASSWORD" ]
 then
-	echo "No WordPress credentials specified. Set ADMIN_USERNAME and ADMIN_PASSWORD."
-	exit 1
+  echo "No WordPress credentials specified. Set ADMIN_USERNAME and ADMIN_PASSWORD."
+  exit 1
 fi
 
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -24,23 +25,8 @@ echo "Behat test site: $TERMINUS_SITE.$TERMINUS_ENV"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo
 
-# Clear site cache
-terminus -n env:clear-cache $TERMINUS_SITE.$TERMINUS_ENV
-
-# Stash current WordPress username
-export WORDPRESS_USER_NAME=$ADMIN_USERNAME
-
-# Use a generic Pantheon user for testing
-export ADMIN_USERNAME='pantheon-ci-testing'
-
-# Check if an admin user with our desired username exists
-ADMIN_USER_EXISTS=$(terminus -n wp ${TERMINUS_SITE}.${TERMINUS_ENV} -- user list --login=${ADMIN_USERNAME} --format=count)
-
-# If so, delete the existing admin user
-if [[ "$ADMIN_USER_EXISTS" == "1" ]]
-then
-  terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- user delete $ADMIN_USERNAME --yes
-fi
+# Delete the admin user if exists
+terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- user delete $ADMIN_USERNAME --yes
 
 # Update WordPress database
 terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- core update-db
@@ -49,10 +35,13 @@ terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- core update-db
 terminus -n backup:create $TERMINUS_SITE.$TERMINUS_ENV
 
 # Create the desired admin user
-terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- user create $ADMIN_USERNAME no-reply@getpantheon.com --user_pass=$ADMIN_PASSWORD --role=administrator --porcelain
+terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- user create $ADMIN_USERNAME no-reply@getpantheon.com --user_pass=$ADMIN_PASSWORD --role=administrator
 
 # Confirm the admin user exists
 terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- user list --login=$ADMIN_USERNAME
+
+# Clear site cache
+terminus -n env:clear-cache $TERMINUS_SITE.$TERMINUS_ENV
 
 # Dynamically set Behat configuration parameters
 export BEHAT_PARAMS='{"extensions":{"Behat\\MinkExtension":{"base_url":"https://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io"},"PaulGibbs\\WordpressBehatExtension":{"site_url":"https://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/wp","users":{"admin":{"username":"'$ADMIN_USERNAME'","password":"'$ADMIN_PASSWORD'"}},"wpcli":{"binary":"terminus -n wp '$TERMINUS_SITE'.'$TERMINUS_ENV' --"}}}}'
@@ -74,7 +63,4 @@ echo "\n Starting Chrome in headless mode ..."
 google-chrome-unstable --disable-gpu --headless --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --no-sandbox </dev/null &>/dev/null &
 
 # Run the Behat tests
-./vendor/bin/behat --config=tests/behat/behat-pantheon.yml --strict "$@"
-
-# Reset WordPress user name
-export ADMIN_USERNAME=$WORDPRESS_USER_NAME
+./vendor/bin/behat --config=tests/behat/behat-pantheon.yml --strict
