9cba1a1c7c214f3b51f23789dc2af6d53729ef18
Carl Alberto <carl.alberto@getpantheon.com>
2020-04-02 10:47:51 +0800

    removing exposed salts

--- web/wp-config.php
+++ web/wp-config.php
@@ -85,14 +85,14 @@ if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
 	 *
 	 * @since 2.6.0
 	 */
-	define( 'AUTH_KEY', '^J/U[^{cOJxhcmmCq2MX%nUs}i_^7nm[w+VsetLZ[JXW9Un/IiyWVEXk;s}X=?u$' );
-	define( 'SECURE_AUTH_KEY', 'dT,wlW20L5V3ChmTEHFGVtUE-r&A)y+G%Pnql&eKdVAWvdIr9FO4lh_Gc9ZVn!1|' );
-	define( 'LOGGED_IN_KEY', '{0E{}k_e7!XRt*}h}nuMP[sKn$gb(O@|[>?bUs}B{>:|+|lL%czE/!Tlc Uk53#:' );
-	define( 'NONCE_KEY', '|M9$H1t9D@AR6>JM[]?9RoA^dmOCHt6ldAE%x|0 Iqpi+m32>1>?0*_?*#|6f7|W' );
-	define( 'AUTH_SALT', 'CA-BmAsS|o_P|!I8Wfu%a=qXC;!3p[8]W_:N2{oI]HhpLP(%2]zWLH+aHTHDw9>%' );
-	define( 'SECURE_AUTH_SALT', 'pi-EA,AOXk*U[VZ|t]R;@K<WMcbD)>k* ;8+hKX:A|$.Z@HL@0`SE?W0:-?-IRd!' );
-	define( 'LOGGED_IN_SALT', 'e+6%u)u@RZn-$}_Q[N;Na<|A-[Am_$#nhD~}ci:%R&B*oiq<sPF$v)d1r<-V-5W|' );
-	define( 'NONCE_SALT', 'r%oyx_`[A-~<LB)]I.,^//}/&]a)H|fzk3IUWrZn[L4qf#Pp#lsB-B}+/ai&u,/|' );
+	define( 'AUTH_KEY', '' );
+	define( 'SECURE_AUTH_KEY', '' );
+	define( 'LOGGED_IN_KEY', '' );
+	define( 'NONCE_KEY', '' );
+	define( 'AUTH_SALT', '' );
+	define( 'SECURE_AUTH_SALT', '' );
+	define( 'LOGGED_IN_SALT', '' );
+	define( 'NONCE_SALT', '' );
 
 endif;
 
